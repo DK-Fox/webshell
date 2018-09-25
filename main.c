81dfd8011f945dbef918a9456953d745a9f33056
DK-Fox <syc0441@163.com>
2018-09-25 22:10:36 +0800

    start

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * ******************************************************
+ * @ Filename:main.c
+ * @ Author:syc
+ * @ Description:This is the main program of the server, which is used to parse the command.
+ * ******************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("start...\n");
+    exit(0);
+}
