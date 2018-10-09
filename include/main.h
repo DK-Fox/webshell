f9551af3c89df46c7bc5d2478b90ece994ac6b09
DK-Fox <syc0441@163.com>
2018-10-10 02:55:00 +0800

    frame2

---
+++ include/main.h
@@ -0,0 +1,25 @@
+/*
+ * ***************************************************
+ * @ Filename:main.h
+ * @ Author:syc
+ * @ Description:This is the header file of main.c
+ * ***************************************************
+ */
+
+#ifndef __MAIN__
+#define __MAIN__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include "build_in.h"
+#include "apue_err.h"
+
+#define MAXLINE 4096
+
+typedef PArgc int
+typedef PArgv char**
+
+#endif
--- main.c
+++ main.c
@@ -6,37 +6,78 @@
  * ******************************************************
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <limits.h>
-#include "build_in.h"
-#include "apue_err.h"
+#include "main.h"
+
+char line_buf[MAXLINE];
 
 int main(){
-    printf("start...\n");
+    printf("My webshell starts ...\n");
 
-   // char line_buf[MAX_SIZE];
+    PArgc * pargc=(PArgc*)malloc(sizeof(Pargc));
+    PArgv * pargv=(PArgv*)malloc(sizeof(PArgv));
+
     //while(1){
         print_pr();
-        //readline();
+        readline(pargc,pargv);
         //parse_command();
         //external_cmd();
     //}
     exit(0);
 }
+
 /*
  * Print prompt.
  */
-void print_pr(){
+int print_pr(){
     char hostname[HOST_NAME_MAX];
-    gethostname(hostname,HOST_NAME_MAX-1);
+    if(gethostname(hostname,HOST_NAME_MAX-1)!=0)
+        err_sys("gethostname failed")
 
     char * logname=getlogin();
-    char * path=getcwd(NULL,0);
-    printf("%s@%s:%s$ ",logname,hostname,path);
+    if(logname==NULL)
+        err_sys("getlogin failed");
 
+    char * path=getcwd(NULL,0);
+    if(path==NULL)
+        err_sys("getcwd failed");
+
+    printf("%s@%s:%s$ ",logname,hostname,path);
+    return 0;
 }
+
+int readline(PArgc *argc,PArgv * argv){
+    fgets(&line_buf, MAXLINE, stdin);
+    int cnt=0;
+    int length=strlen(line_buf);
+    for(int i=0;i<length;i++){
+        if(strch("\"\'<\(\{\[",line_buf[i])!=NULL && line_buf[i-1]!='\\'){
+            while((char * pch=strch(&line_buf[i+1],line_buf[i]))!=NULL && *(pch-1)=='\\');
+            if(pch==NULL){
+                ;
+                return -1;
+            }else{
+                i+=pch-&line_buf[i];
+                cnt++;
+                continue;
+            }
+        }
+
+        if(line_buf[i]==' '||line_buf[i]=='\t')
+            line_buf[i]='\0';
+
+        if(line_buf[i-1]==' '||line_buf[i]=='\t')
+            cnt++;
+    }
+    *argc=cnt;
+    *argv=(Pargv)malloc(cnt*sizeof(*Pargv));
+    for(int i=0,cnt=0;i<length;i++){
+        if(line_buf[i]!='\0',line_buf[i-1]=='\0'){
+            (*argv)[cnt++]=&line_buf[i];
+        }
+    }
+    return 0;
+}
+
+
 
 
