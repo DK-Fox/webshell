4f164d5813e3d767ede5115b65e557cd975b392e
DK-Fox <syc0441@163.com>
2018-11-13 02:58:38 +0800

    v1.0

--- cal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-NUM_C=$(find . -name "*.c"|xargs wc -l|grep total|tr -s ' '|cut -d ' ' -f2)
-NUM_H=$(find . -name "*.h"|xargs wc -l|grep total|tr -s ' '|cut -d ' ' -f2)
-
-echo $((${NUM_C}+${NUM_H}))
-
